c654c18e17903a0799d090aeadfe8ceae29eecb0
keni-77 <kenikeni2024@outlook.com>
2026-03-02 13:12:59 +0900

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,31 +1,22 @@
 // signup.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 import { firebaseConfig } from "./firebaseConfig.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
 window.signup = async function () {
-  const username = document.getElementById("username").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  if (!username || !email || !password) {
-    alert("すべて入力してください");
+  if (!email || !password) {
+    alert("メールアドレスとパスワードを入力してください");
     return;
   }
 
   try {
-    const userCred = await createUserWithEmailAndPassword(auth, email, password);
-
-    await setDoc(doc(db, "users", userCred.user.uid), {
-      username,
-      email
-    });
-
+    await createUserWithEmailAndPassword(auth, email, password);
     window.close();
   } catch (err) {
     console.error(err);
