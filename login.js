20933ffe010dcad2ab5aa9a3d47af17e5c5f7500
keni-77 <kenikeni2024@outlook.com>
2026-03-02 13:26:20 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,24 +1,16 @@
 // login.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
 import { firebaseConfig } from "./firebaseConfig.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-window.login = function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  if (!email || !password) {
-    alert("メールアドレスとパスワードを入力してください");
-    return;
-  }
-
-  signInWithEmailAndPassword(auth, email, password)
+window.googleLogin = function () {
+  signInWithPopup(auth, provider)
     .then(() => {
-      // ログイン成功 → このタブを閉じる
       window.close();
     })
     .catch(err => {
